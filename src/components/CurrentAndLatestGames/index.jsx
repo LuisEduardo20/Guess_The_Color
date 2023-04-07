713b875400bf7ae33a40ec3c04742f04e1b1212e
Luis Eduardo <luiseduardo20.dev@gmail.com>
2023-04-07 11:39:26 -0300

    feat: aside navigation game history component

---
+++ src/components/CurrentAndLatestGames/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss";
+
+const CurrentAndLatestGames = () => {
+  return (
+    <div className='aside-section'>
+      <h4>Current/Latest game's</h4>
+    </div>
+  );
+};
+
+export default CurrentAndLatestGames;
