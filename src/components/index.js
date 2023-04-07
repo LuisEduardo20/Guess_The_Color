22335395c11690521343e6ba918cc8fb691ce858
Luis Eduardo <luiseduardo20.dev@gmail.com>
2023-04-07 11:38:38 -0300

    feat: export components index

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as CurrentAndLatestGames } from "./CurrentAndLatestGames";
+export { default as MainContent } from "./MainContent";
+export { default as GameHeader } from "./GameHeader";
+export { default as GameBody } from "./GameBody";
