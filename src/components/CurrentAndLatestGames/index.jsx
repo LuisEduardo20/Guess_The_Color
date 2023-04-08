4b2010b5001f60333cd50ab2e1514331af9f4be4
Luis Eduardo <luiseduardo20.dev@gmail.com>
2023-04-08 17:33:04 -0300

    feat: list previous questions answers

--- src/components/CurrentAndLatestGames/index.jsx
+++ src/components/CurrentAndLatestGames/index.jsx
@@ -1,10 +1,89 @@
 import React from "react";
+import { AiFillCheckCircle, AiFillCloseCircle } from "react-icons/ai";
+import fontColorContrast from "font-color-contrast";
+
+import { useGame } from "../../hooks/useGame";
+
 import "./styles.scss";
 
 const CurrentAndLatestGames = () => {
+  const { oldQuestionsArr } = useGame();
+
   return (
     <div className='aside-section'>
-      <h4>Current/Latest game's</h4>
+      <h4>Previous questions</h4>
+
+      <ul className='previous-answers-list'>
+        {oldQuestionsArr.map((oldQuestion) => {
+          return (
+            <li>
+              <div className='answers-container'>
+                <div
+                  style={{
+                    backgroundColor: oldQuestion.questionAnswer,
+                  }}
+                  className='answer'
+                >
+                  <h5
+                    style={{
+                      color: fontColorContrast(
+                        oldQuestion.questionAnswer
+                      ),
+                    }}
+                  >
+                    Right answer
+                  </h5>
+                  <p
+                    style={{
+                      color: fontColorContrast(
+                        oldQuestion.questionAnswer
+                      ),
+                    }}
+                  >
+                    {oldQuestion.questionAnswer}
+                  </p>
+                </div>
+
+                {!oldQuestion.hitQuestion && (
+                  <div
+                    style={{
+                      backgroundColor: oldQuestion.clickedAnswer,
+                    }}
+                    className='answer'
+                  >
+                    <h5
+                      style={{
+                        color: fontColorContrast(
+                          oldQuestion.clickedAnswer
+                        ),
+                      }}
+                    >
+                      Wrong answer
+                    </h5>
+                    <p
+                      style={{
+                        color: fontColorContrast(
+                          oldQuestion.clickedAnswer
+                        ),
+                      }}
+                    >
+                      {oldQuestion.clickedAnswer}
+                    </p>
+                  </div>
+                )}
+              </div>
+
+              <div className='icon-container'>
+                {oldQuestion.hitQuestion ? (
+                  <AiFillCheckCircle color='green' size={25} />
+                ) : (
+                  <AiFillCloseCircle color='red' size={25} />
+                )}
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
