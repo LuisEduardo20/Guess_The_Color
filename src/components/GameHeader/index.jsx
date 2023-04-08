cef14346d50d7b2b6c23632bb2466b1d1f2d8069
Luis Eduardo <luiseduardo20.dev@gmail.com>
2023-04-07 22:57:16 -0300

    feat: game header

--- src/components/GameHeader/index.jsx
+++ src/components/GameHeader/index.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { usePlayer } from "../../hooks/usePlayer";
+import { useGame } from "../../hooks/useGame";
+
+import "./styles.scss";
 
 const GameHeader = () => {
-  return <div>GameHeader</div>;
+  const { highScore, playerScore } = usePlayer();
+  const { gameTime, handleRestartGame } = useGame();
+
+  return (
+    <div className='header'>
+      <section className='remaining-time'>
+        <h5>Remaining Time(s)</h5>
+        <p>{gameTime}s</p>
+      </section>
+
+      <section className='restart'>
+        <button onClick={handleRestartGame}>Restart</button>
+      </section>
+
+      <section className='score'>
+        <div>
+          <h5>High Score</h5>
+          <p>{highScore ? highScore.score : "Empty"}</p>
+        </div>
+
+        <div>
+          <h5>Score</h5>
+          <p>{playerScore}</p>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default GameHeader;
