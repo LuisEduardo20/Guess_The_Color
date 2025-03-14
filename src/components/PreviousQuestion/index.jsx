2a77d8e39b57f80ef43ff31f8445dcce1f572208
Luis Eduardo <luiseduardo20.dev@gmail.com>
2025-03-14 17:38:07 -0300

    fix: build problem

--- src/components/PreviousQuestion/index.jsx
+++ src/components/PreviousQuestion/index.jsx
@@ -1,17 +1,17 @@
 import fontColorContrast from "font-color-contrast";
-import React, { useCallback, useMemo } from "react";
+import React, { useMemo } from "react";
 import { AiFillCheckCircle, AiFillCloseCircle } from "react-icons/ai";
 
 const PreviousQuestion = ({ question }) => {
   const renderData = useMemo(() => {
     return (
       <li>
-        <div className='answers-container'>
+        <div className="answers-container">
           <div
             style={{
               backgroundColor: question.questionAnswer,
             }}
-            className='answer'
+            className="answer"
           >
             <h5
               style={{
@@ -34,7 +34,7 @@ const PreviousQuestion = ({ question }) => {
               style={{
                 backgroundColor: question.clickedAnswer,
               }}
-              className='answer'
+              className="answer"
             >
               <h5
                 style={{
@@ -54,11 +54,11 @@ const PreviousQuestion = ({ question }) => {
           )}
         </div>
 
-        <div className='icon-container'>
+        <div className="icon-container">
           {question.hitQuestion ? (
-            <AiFillCheckCircle color='#01966e' size={25} />
+            <AiFillCheckCircle color="#01966e" size={25} />
           ) : (
-            <AiFillCloseCircle color='#ef476f' size={25} />
+            <AiFillCloseCircle color="#ef476f" size={25} />
           )}
         </div>
       </li>
